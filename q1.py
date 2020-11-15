a5ba36780b350541834087c372b500659a03ca65
Ayushi Pandey <41800690+ayushipandey5@users.noreply.github.com>
2020-11-15 23:07:02 +0530

    q1 for 3c

--- q1.py
+++ q1.py
@@ -19,50 +19,87 @@ for L in data:
             levels[elements.index(dicts["name"])] = int(L[1])
 
 min_level = min(levels)
-if min_level == 0:
-    print("No leader")
-else:
-    par_at_min_level = [i for i in levels]
-    parents = [i for i in elements]
+par_at_min_level = [i for i in levels]
+parents = [i for i in elements]
+
+
+def levelMatch(plevel,i):
+    while plevel != min_level:
+        for L in data:
+            for dicts in data[L]:
+                if dicts["name"] == parents[i]:
+                    if int(L[1]) > 0:
+                        plevel = int(L[1]) -1
+                        parents[i] = dicts["parent"]
+    return
+
+def First():
+    res = False
     for i in range(size):
         if par_at_min_level[i] != min_level:
             plevel = par_at_min_level[i]
-            while plevel != min_level:
+            levelMatch(plevel,i)
+            par_at_min_level[i] = plevel
+
+        res = all(ele == parents[0] for ele in parents)
+    return res
+
+def ifParentDiff(res):
+    while(res == False):
+        for i in range(size):
+            if par_at_min_level[i] > 0:
                 for L in data:
                     for dicts in data[L]:
                         if dicts["name"] == parents[i]:
                             if int(L[1]) > 0:
                                 plevel = int(L[1]) -1
                                 parents[i] = dicts["parent"]
-            par_at_min_level[i] = plevel
+            par_at_min_level[i] = plevel                
+        res = all(ele == parents[0] for ele in parents)
+    return res
 
-    res = all(ele == parents[0] for ele in parents)
+def Second(res):
     if (res == False):
-        while(res == False):
-            for i in range(size):
-                if par_at_min_level[i] > 0:
-                    for L in data:
-                        for dicts in data[L]:
-                            if dicts["name"] == parents[i]:
-                                if int(L[1]) > 0:
-                                    plevel = int(L[1]) -1
-                                    parents[i] = dicts["parent"]
-                par_at_min_level[i] = plevel                
-            res = all(ele == parents[0] for ele in parents)
+        res = ifParentDiff(res)
         print("Common leader : " + parents[0])
         for i in range(size):
             print("Leader " + parents[0] + " is " + str(levels[i] - par_at_min_level[i]) +" levels above employee " + elements[i])
-        
-    elif (res):
+        exit()
+    return res
+
+def ifParentSame(i):
+    if par_at_min_level[i] > 0:
+        for L in data:
+            for dicts in data[L]:
+                    if dicts["name"] == parents[i]:
+                        if int(L[1]) > 0:
+                            plevel = int(L[1]) -1
+                            parents[i] = dicts["parent"]
+        par_at_min_level[i] = plevel 
+    return
+
+
+def Third(res):
+    if (res):
         for i in range(size):
-            if par_at_min_level[i] > 0:
-                for L in data:
-                    for dicts in data[L]:
-                            if dicts["name"] == parents[i]:
-                                if int(L[1]) > 0:
-                                    plevel = int(L[1]) -1
-                                    parents[i] = dicts["parent"]
-                par_at_min_level[i] = plevel 
+            ifParentSame(i)
         print("Common leader : " + parents[0])
         for i in range(size):
-            print("Leader " + parents[0] + " is " + str(levels[i] - par_at_min_level[i]) +" levels above employee " + elements[i])
+            print("Leader " + parents[0] + " is " + str(levels[i] - par_at_min_level[i]) +" levels above employee " + elements[i])    
+        exit()
+    return
+
+
+
+def Solution():
+    if min_level == 0:
+        print("No leader")
+    else:
+        res = First()
+        res = Second(res)        
+        Third(res)
+
+    return
+
+
+Solution()
